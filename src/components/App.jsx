9b7cb99724e98a9f1e4f79132df9037f724b1771
Perslay <natalia.44.fedyk@gmail.com>
2024-05-22 20:42:50 +0200

    Base
    From previous repository

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,18 @@
+// import { useState, useEffect } from 'react';
+// import { nanoid } from 'nanoid';
+import css from '../styles/App.module.css';
+import { ContactList } from './ContactList';
+import { ContactForm } from './ContactForm';
+import { Filter } from './Filter';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className={css.appContainer}>
+      <h1 className={css.firstHeading}>Phonebook</h1>
+      <ContactForm />
+      <h2 className={css.secondHeading}>Contacts</h2>
+      <Filter />
+      <ContactList />
     </div>
   );
 };
---
+++ src/components/ContactList.jsx
@@ -0,0 +1,27 @@
+import { useSelector } from 'react-redux';
+import { getContacts, getFilter } from '../redux/selectors';
+import { Contact } from './Contact';
+import css from '../styles/ContactList.module.css';
+
+export const ContactList = () => {
+  const contacts = useSelector(getContacts);
+  const filter = useSelector(getFilter);
+
+  const getVisibleContacts = (contacts, filter) => {
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+  };
+
+  const visibleContacts = getVisibleContacts(contacts, filter);
+
+  return (
+    <ul className={css.list}>
+      {visibleContacts.map(contact => (
+        <li className={css.listItem} key={contact.id}>
+          <Contact contact={contact} />
+        </li>
+      ))}
+    </ul>
+  );
+};
